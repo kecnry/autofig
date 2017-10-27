7883cfb52ad2d10b71ed40e7b3212f3eb650ad42
Kyle Conroy <kyleconroy@gmail.com>
2017-10-27 12:44:49 -0400

    add limits nosetest

---
+++ tests/test_limits.py
@@ -0,0 +1,43 @@
+import autofig
+from nose.tools import assert_raises
+
+def test_limits():
+    autofig.reset()
+
+    autofig.plot(x=[0,10], y=[0,10])
+    autofig.plot(x=[-5,5], y=[-5,5])
+
+    assert(autofig.gcf().axes[0].x.lim == (-5,10))
+    assert(autofig.gcf().axes[0].y.lim == (-5,10))
+
+def test_padding():
+    autofig.reset()
+
+    autofig.plot(x=[0,10], y=[0,10])
+    autofig.plot(x=[-5,5], y=[-5,5])
+
+    autofig.gcf().axes[0].pad = 0.1
+
+    assert(autofig.gcf().axes[0].x.lim == (-6.5,11.5))
+    assert(autofig.gcf().axes[0].y.lim == (-6.5,11.5))
+
+    autofig.gcf().axes[0].y.pad = 0.0
+
+    assert(autofig.gcf().axes[0].x.lim == (-6.5,11.5))
+    assert(autofig.gcf().axes[0].y.lim == (-5,10))
+
+
+def test_consider_for_limits():
+    autofig.reset()
+
+    autofig.plot(x=[0,10], y=[0,10])
+    autofig.plot(x=[-5,5], y=[-5,5])
+    autofig.plot(x=[-30,30], y=[-30,30], consider_for_limits=False)
+
+    assert(autofig.gcf().axes[0].x.lim == (-5,10))
+    assert(autofig.gcf().axes[0].y.lim == (-5,10))
+
+if __name__ == '__main__':
+    test_limits()
+    test_padding()
+    test_consider_for_limits()
