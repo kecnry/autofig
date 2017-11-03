05c80cb4e8a29cae01057b86a3815487a15d954c
Kyle Conroy <kyleconroy@gmail.com>
2017-11-03 14:49:43 -0400

    colormap cycling (#13)
    * implement cmap cycler (in an MPL-version-safe way)
    * update inconsistency checks

--- tests/test_color_inconsistencies.py
+++ tests/test_color_inconsistencies.py
@@ -9,27 +9,18 @@ def test_unit_inconsistencies():
 
     autofig.reset()
     autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='solRad')
-    # these have different units but are trying to use the same (default) cmap
-    # this may be "fixed" in the future with a cmap cycler
-    assert_raises(ValueError, autofig.plot, x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
-    # assert(len(autofig.gcf().axes[0].cs)==2)
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
+    assert(len(autofig.gcf().axes[0].cs)==2)
 
     autofig.reset()
     autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # these have different units but are trying to use the same (default) cmap
-    # this may be "fixed" in the future with a cmap cycler
-    assert_raises(ValueError, autofig.plot, x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
-    # assert(len(autofig.gcf().axes[0].cs)==2)
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='kg')
+    assert(len(autofig.gcf().axes[0].cs)==2)
 
     autofig.reset()
     autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cunit='solRad')
-    # these have different units but are trying to use the same (default) cmap
-    # this may be "fixed" in the future with a cmap cycler
-    assert_raises(ValueError, autofig.plot, x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # assert(len(autofig.gcf().axes[0].cs)==2)
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
+    assert(len(autofig.gcf().axes[0].cs)==2)
 
 def test_label_inconsistencies():
     autofig.reset()
@@ -46,25 +37,21 @@ def test_label_inconsistencies():
 
     autofig.reset()
     autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], clabel='blah')
-    # these have different labels but are trying to use the same (default) cmap
-    # this may be "fixed" in the future with a cmap cycler
-    assert_raises(ValueError, autofig.plot, x=[1,2,3], y=[1,2,3], c=[1,2,3], clabel='OTHER')
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], clabel='OTHER')
-    # assert(len(autofig.gcf().axes[0].cs)==2)
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], clabel='OTHER')
+    assert(len(autofig.gcf().axes[0].cs)==2)
 
 def test_cmap_inconsistencies():
 
-    # TODO: fix and enable this test
-    # autofig.reset()
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # assert(len(autofig.gcf().axes[0].cs)==1)
+    autofig.reset()
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
+    assert(len(autofig.gcf().axes[0].cs)==1)
 
-    # TODO: fix and enable this test
-    # autofig.reset()
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cmap='brg')
-    # autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
-    # assert(len(autofig.gcf().axes[0].cs)==2)
+    # TODO: not sure whether I think this should cause an inconsistency or not...
+    autofig.reset()
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3], cmap='brg')
+    autofig.plot(x=[1,2,3], y=[1,2,3], c=[1,2,3])
+    assert(len(autofig.gcf().axes[0].cs)==1)
 
 
     autofig.reset()
