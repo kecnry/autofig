5259869429458c9eb011226a764658a790488ce0
Kyle Conroy <kyleconroy@gmail.com>
2018-02-26 18:17:02 -0500

    add BASIC mesh nosetest
    just tests to make sure a single triangle can be plotted
    
    closes #31

---
+++ tests/test_mesh.py
@@ -0,0 +1,17 @@
+import autofig
+import numpy as np
+from nose.tools import assert_raises
+
+def test_mesh(show=False):
+    autofig.reset()
+
+    x = np.array([np.array([0,1,1.5])])
+    y = np.array([np.array([0,0,1])])
+    z = np.array([np.array([0,0,0])])
+
+    autofig.mesh(x=x, y=y, z=z)
+
+    autofig.draw(show=show)
+
+if __name__ == '__main__':
+    test_mesh(show=True)
