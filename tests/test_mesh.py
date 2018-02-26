1dd45235afdac9873b728fc3a2bbcffdf45570d2
Kyle Conroy <kyleconroy@gmail.com>
2018-02-26 18:46:19 -0500

    implement support for passing linestyle as an argument to mesh (#34)
    * implemented linestyle as argument to Mesh
    * check if linestyle is valid according to the cycler

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -9,7 +9,10 @@ def test_mesh(show=False):
     y = np.array([np.array([0,0,1])])
     z = np.array([np.array([0,0,0])])
 
-    autofig.mesh(x=x, y=y, z=z)
+    assert_raises(ValueError, autofig.mesh, x, y, z, linestyle='blue')
+
+    autofig.reset()
+    autofig.mesh(x=x, y=y, z=z, linestyle='dashed')
 
     autofig.draw(show=show)
 
