462095153b0e7ed849368a08775c2f1d5ec3ba75
Kyle Conroy <kyleconroy@gmail.com>
2018-10-25 12:18:46 -0400

    update versioning for 1.0 release

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='autofig',
-      version='0.1.0',
-      description='AutoFig',
+      version='1.0.0',
+      description='autofig: high-level wrapper for creating animations and figures with matplotlib',
       author='Kyle Conroy',
       author_email='kyleconroy@gmail.com',
       url='https://www.github.com/kecnry/autofig',
+      download_url = 'https://github.com/kecnry/autofig/tarball/1.0.0',
       packages=['autofig'],
      )
